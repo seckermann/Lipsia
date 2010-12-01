d2ca91373e5db6401a06faf508917199aa7a630a
tuerke <tuerke@951fd081-0026-45af-85c1-ca8f1b9ba834>
2010-12-01 12:17:09 +0000

    if there are more than one image to write the output name is generated as the chronological order of the sequences

--- src/new/vvinidi/isisconv.cpp
+++ src/new/vvinidi/isisconv.cpp
@@ -1,6 +1,10 @@
 #include <DataStorage/io_application.hpp>
 #include <DataStorage/io_factory.hpp>
+#include <DataStorage/image.hpp>
 #include <boost/foreach.hpp>
+#include <boost/filesystem.hpp>
+#include <boost/date_time/posix_time/posix_time.hpp>
+#include <list>
 
 
 using namespace isis;
@@ -22,7 +26,32 @@ int main( int argc, char **argv )
 			ref->setProperty<u_int16_t>( "repetitionTime", app.parameters["tr"]->as<double>() * 1000 );
 		}
 	}
-
-	app.autowrite( app.images );
+	if( app.images.size() > 1 ) {
+		typedef std::multimap< boost::posix_time::ptime, boost::shared_ptr<data::Image> > timeStampsType;
+		timeStampsType timeStamps;
+		BOOST_FOREACH( std::list<boost::shared_ptr<data::Image> >::const_reference image, app.images ) 
+		{
+			timeStamps.insert( std::make_pair< boost::posix_time::ptime, boost::shared_ptr< data::Image > >(
+				image->getProperty< boost::posix_time::ptime >("sequenceStart"), image ) ) ;
+		}
+		if ( timeStamps.size() != app.images.size() ) 
+		{
+			LOG( ImageIoLog, warning ) << "Number of images and number of different timestamps do not coincide!"
+											 << timeStamps.size() << " != " << app.images.size();
+		}
+		size_t count = 1;
+		BOOST_FOREACH( timeStampsType::const_reference map, timeStamps )
+		{
+			boost::filesystem::path out( app.parameters["out"].toString() );
+			std::stringstream newFileName;
+			newFileName << out.branch_path() << "/S" << count++ << "_" << out.leaf();
+			data::ImageList tmpList;
+			tmpList.push_back( map.second );
+			data::IOFactory::write(tmpList, newFileName.str(), "", "");
+		}
+	} else {
+		app.autowrite( app.images );
+	}
+	
 	return EXIT_SUCCESS;
 }
