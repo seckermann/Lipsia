d1715af5b85f73bae7117778a44e948b4ee8fa44
Erik Tuerke <tuerke@cbs.mpg.de>
2011-10-05 13:39:37 +0200

    see last commit

--- src/old/vmovcorrection/vmovcorrection.c
+++ src/old/vmovcorrection/vmovcorrection.c
@@ -36,9 +36,9 @@
 #include <math.h>
 #include <stdlib.h>
 
-#ifdef _OPENMP
+/* #ifdef _OPENMP
 #include <omp.h>
-#endif /*OPENMP */
+ #endif /*OPENMP */
 
 extern void VSpatialFilter1(VAttrList);
 extern VImage VMotionCorrection3d(VAttrList, VLong, VLong, VLong);
@@ -73,8 +73,8 @@ main(int argc, char *argv[]) {
         {"tref", VLongRepn, 1, (VPointer) &i0, VOptionalOpt, NULL, "reference time step"},
         {"report", VStringRepn, 1, (VPointer) &filename, VOptionalOpt, NULL, "report file"},
         {"step", VLongRepn, 1, (VPointer) &step, VOptionalOpt, NULL, "Step size (e.g. 2 or 3)"},
-        {"iterations", VLongRepn, 1, (VPointer) &maxiter, VOptionalOpt, NULL, "Max number of iterations"},
-	{"j", VShortRepn, 1,(VPointer) &nproc, VOptionalOpt, NULL, "number of processors to use, '0' to use all"}
+        {"iterations", VLongRepn, 1, (VPointer) &maxiter, VOptionalOpt, NULL, "Max number of iterations"}/*,
+	{"j", VShortRepn, 1,(VPointer) &nproc, VOptionalOpt, NULL, "number of processors to use, '0' to use al l"} */
     };
     FILE *in_file = NULL, *out_file = NULL;
     VImage motion = NULL;
@@ -93,7 +93,7 @@ main(int argc, char *argv[]) {
 
 
 /*openmp stuff */
-#ifdef _OPENMP
+/*#ifdef _OPENMP
     int num_procs=omp_get_num_procs();
     if (nproc > 0 && nproc < num_procs) num_procs = nproc;
     printf("using %d cores\n",(int)num_procs);
